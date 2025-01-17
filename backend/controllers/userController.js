fe136e4ba65a5d608ae04f605dc429a2083c1d1b
Abhinavasujeevan <141955543+Abhinavasujeevan@users.noreply.github.com>
2025-01-17 12:02:09 +0530

    Add files via upload

---
+++ backend/controllers/userController.js
@@ -0,0 +1,75 @@
+import userModel from "../models/usermodels";
+import jwt from "jasonwebtoken"
+import bcrypt from "bcrypt"
+import validator from "validator"
+
+
+//login user
+const loginUser = async (req,res) => {
+    const {email,password} = req.body;
+    try {
+        const user = await userModel.findOne({email});
+
+        if (!user) {
+            return res.json({success:false,message:"user doesn't exists"})
+        
+        }
+        const isMatch = await bcrypt.compare(password,user.password)
+
+        if (!isMatch) {
+            return res.json({success:false, message:"invalid credentials"})
+
+        }
+
+        const token = createToken(user._id);
+        res.json({success:true, token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+        
+    }
+}
+const createToken = (id) => {
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
+//regiester user
+const registerUser = async (req,res) => {
+    const {name,password,email} = req.body;
+    try {
+        // cheaking is user alrady exists
+        const exists = await userModel.findOne({email})
+        if(exists){
+            return res.json({success:false,message: "user already exists"})
+        }
+        // validating email format and string password
+        if(!validator.isEmail(email)){
+            return res.json({success:false,message: "please enter a vaild email"})
+        }
+        if(password.length < 8){
+            return res.json({success:false,message:"please enter strong password"})
+        }
+
+        // hashing user password
+        const satl = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword
+        })
+
+        const user =  await newUser.save()
+        const token = createToken(user._id)
+        res.json({success:true, token});
+
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+
+    }
+} 
+
+export {loginUser,registerUser}
