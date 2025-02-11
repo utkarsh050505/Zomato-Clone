41cfb25a4d2f5edf8ca948fc5530dd1dcc76c4c1
Utkarsh Mandal <utkarshmandal420@gmail.com>
2025-02-11 16:41:28 +0530

    Complete

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -4,11 +4,15 @@ import Sidebar from './components/Sidebar/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
-import Order from './pages/Orders/Order'
+// import Order from './pages/Orders/Order'
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Orders from './pages/Orders/Orders'
 
 const App = () => {
+
+  const url = 'http://localhost:4000'
+
   return (
     <div>
       <ToastContainer/>
@@ -17,10 +21,9 @@ const App = () => {
       <div className='app-content'>
         <Sidebar/>
         <Routes>
-          <Route path="/add" element={<Add/>}/>
-          <Route path="/list" element={<List/>}/>
-          <Route path="/orders" element={<Order/>}/>
-
+          <Route path="/add" element={<Add url={url}/>}/>
+          <Route path="/list" element={<List url={url}/>}/>
+          <Route path='/orders' element={<Orders url={url}/>}/>
         </Routes>
       </div>
     </div>
---
+++ Admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import './Orders.css'
+import { useState } from 'react'
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
+import { assets } from '../../assets/assets';
+import { useTheme } from '@emotion/react';
+
+const Orders = ({url}) => {
+
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + '/api/order/list')
+    if (response.data.success) {
+      setOrders(response.data.data);
+    }
+    else {
+      toast.error("Error")
+    }
+  }
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, [])
+
+  return (
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <div key={index} className="order-item">
+            <img src={assets.parcel_icon} alt='' className='img'/>
+            <div>
+              <p className='order-item-food'>
+                {order.items.map((item, idx) => 
+                  `${item.name} x ${item.quantity}${idx === order.items.length - 1 ? "" : ", "}`
+                )}
+              </p>
+              <p className='order-item-name'>
+                {order.address.firstName + " " + order.address.lastName}
+              </p>
+              <div className="order-item-address">
+                <p>{order.address.street + ","}</p>
+                <p>
+                  {order.address.city + ", " + order.address.state + ", " + order.address.country + ", " + order.address.zipcode}
+                </p>
+              </div>
+              <p className='order-item-phone'>{order.address.phone}</p>
+              <p>Items: {order.items.length}</p>
+              <p>Amount: ${order.amount}</p>
+              <select className='order-item-select'>
+                <option value="Food Processing">Food Processing</option>
+                <option value="Out for delivery">Out for Delivery</option>
+                <option value="Delivered">Delivered</option>
+              </select>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );  
+}
+
+export default Orders
